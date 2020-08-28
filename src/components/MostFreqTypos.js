171316e593929eb6eba14e6112af153f55535645
bmai <bmai@sfu.ca>
2020-08-28 00:22:53 -0700

    updated layout

--- src/components/MostFreqTypos.js
+++ src/components/MostFreqTypos.js
@@ -13,34 +13,21 @@ const MostFreqTypos = ({ typoMap }) => {
     
     return (
         <div className="MostFreqTypos">
-            <p className="tableTitle">Top 5 Typos</p>
+            <p className="tableTitle">Most Frequent Typos</p>
+
             <table className="table">
-                <thead>
-                    <tr className="tableHeaders">
-                        <th>Word</th>
-                        <th>Count</th>
-                    </tr>
-                </thead>
                 <tbody>
                     <tr>
-                        <td>{typoArray[0] ? typoArray[0].key : "-"}</td>
-                        <td>{typoArray[0] ? typoArray[0].value : "-"}</td>
+                        <td>{typoArray[0] ? typoArray[0].key : ""}</td>
+                        <td className="TypoCount">{typoArray[0] ? `(${typoArray[0].value})` : ""}</td>
                     </tr>
                     <tr>
-                        <td>{typoArray[1] ? typoArray[1].key : "-"}</td>
-                        <td>{typoArray[1] ? typoArray[1].value : "-"}</td>
+                        <td>{typoArray[1] ? typoArray[1].key : ""}</td>
+                        <td className="TypoCount">{typoArray[1] ? `(${typoArray[1].value})` : ""}</td>
                     </tr>
                     <tr>
-                        <td>{typoArray[2] ? typoArray[2].key : "-"}</td>
-                        <td>{typoArray[2] ? typoArray[2].value : "-"}</td>
-                    </tr>
-                    <tr>
-                        <td>{typoArray[3] ? typoArray[3].key : "-"}</td>
-                        <td>{typoArray[3] ? typoArray[3].value : "-"}</td>
-                    </tr>
-                    <tr>
-                        <td>{typoArray[4] ? typoArray[4].key : "-"}</td>
-                        <td>{typoArray[4] ? typoArray[4].value : "-"}</td>
+                        <td>{typoArray[2] ? typoArray[2].key : ""}</td>
+                        <td className="TypoCount">{typoArray[2] ? `(${typoArray[2].value})` : ""}</td>
                     </tr>
                 </tbody>
             </table>
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,24 +4,30 @@ import '../styles/Stats.css'
 const Stats = props => (
 
     <div>
-        <h1 className="Stats">
-            <span style={{ color: `${props.wpmColor}` }}>
-                WPM: {props.wpm == 0 ? '0.00' : props.wpm}
-            </span>
-
-            <span> | </span>
-
-            <span style={{ color: `${props.accuracyColor}` }}>
-                ACC: {props.accuracy == 0 ? '0.00' : props.accuracy}
-            </span>
-        </h1>
-        <h2>
-            TIME: {props.runTime ? props.runTime : '00:00'}
-        </h2>
-        <h2>
-            WORDS: {props.wordCount ? props.wordCount : 0}
-        </h2>
-
+        <table className="Stats">
+            <tbody>
+                <tr className="MainStats">
+                    <td className="StatsLeft">
+                        <div style={{ color: `${props.wpmColor}` }}>
+                            WPM: {props.wpm == 0 ? '0.00' : props.wpm}
+                        </div>
+                    </td>
+                    <td className="StatsRight">
+                        <div style={{ color: `${props.accuracyColor}` }}>
+                            ACC: {props.accuracy == 0 ? '0.00' : props.accuracy}
+                        </div>
+                    </td>
+                </tr>
+                <tr className="SecondaryStats">
+                    <td className="StatsLeft">
+                        WORDS: {props.wordCount ? props.wordCount : 0}
+                    </td>
+                    <td className="StatsRight">
+                        TIME: {props.runTime ? props.runTime : '00:00'}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 )
