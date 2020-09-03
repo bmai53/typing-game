688d0ab61db534256b35748d0e735dc306f8831c
bmai <bmai@sfu.ca>
2020-09-02 19:45:14 -0700

    fix: stutter on first render of typing cat

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -4,12 +4,12 @@ import base from '../bongo_cat/base.png'
 import typing from '../bongo_cat/typing.gif'
 
 export default ({ isTyping, startTime }) => {
-    
+
     const [cur, setCur] = useState('left')
 
     useEffect(() => {
         const typing = setInterval(() => {
-            if(cur === 'left'){
+            if (cur === 'left') {
                 setCur('right')
             }
             else {
@@ -22,14 +22,21 @@ export default ({ isTyping, startTime }) => {
         }
     })
 
-    if (!startTime){
+    if (!startTime) {
         return (
-            <div className="Container">
-                <img className="Cat" src={base} />
-                <div class="ImgText blink">
-                    <p>Type to start the game!</p>
+            <>
+                <div className="Container">
+                    <img className="Cat" src={base} />
+                    <div class="ImgText blink">
+                        <p>Type to start the game!</p>
+                    </div>
                 </div>
-            </div>
+
+                {/* loads image as hidden to prevent stutter when typing is rendered for the first time */}
+                <div style={{ display: "none" }}>
+                    <img className="Cat" src={typing} />
+                </div>
+            </>
         )
     }
     else if (!isTyping) {
